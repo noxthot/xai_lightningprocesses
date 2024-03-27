dfb2bc5a89fcadf2b751689664199637e87d7040
Gregor Ehrensperger <gregor.ehrensperger@datalabhell.at>
2024-03-27 08:38:28 +0100

    fix(utils_plots): add cmap_map #224

--- utils_plots.py
+++ utils_plots.py
@@ -1,6 +1,7 @@
 import pickle
 import random
 
+import matplotlib.colors as mcolors
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -290,3 +291,38 @@ def plot_many_profiles(df_many_cases, target_var, ptype='q90', y_axis='level', s
 
     for g in ggs:    
         g.fig.clf()
+
+
+def cmap_map(function, cmap):
+    cdict = cmap._segmentdata
+    step_dict = {}
+    
+    # First get the list of points where the segments start or end
+    for key in ('red', 'green', 'blue'):
+        step_dict[key] = list(map(lambda x: x[0], cdict[key]))
+        
+    step_list = sum(step_dict.values(), [])
+    step_list = np.array(list(set(step_list)))
+    
+    # Then compute the LUT, and apply the function to the LUT
+    reduced_cmap = lambda step : np.array(cmap(step)[:3])
+    old_LUT = np.array(list(map(reduced_cmap, step_list)))
+    new_LUT = np.array(list(map(function, old_LUT)))
+    
+    # Now try to make a minimal segment definition of the new LUT
+    cdict = {}
+    
+    for i, key in enumerate(['red','green','blue']):
+        this_cdict = {}
+
+        for j, step in enumerate(step_list):
+            if step in step_dict[key]:
+                this_cdict[step] = new_LUT[j, i]
+            elif new_LUT[j, i] != old_LUT[j, i]:
+                this_cdict[step] = new_LUT[j, i]
+
+        colorvector = list(map(lambda x: x + (x[1], ), this_cdict.items()))
+        colorvector.sort()
+        cdict[key] = colorvector
+
+    return mcolors.LinearSegmentedColormap('colormap', cdict, 1024)
